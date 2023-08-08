d987be27cb1d8535685e17603ffb1b24a2e7d5b9
valen-web <128190072+valen-web@users.noreply.github.com>
2023-08-07 22:53:55 -0500

    Resolver conflictos

---
+++ index.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  // nested for loop
+  for (let i = 0; i < 5; i++) {
+    for (let j= 0; j< 5; j++) {
+      console.log(i, j);
+    }
+  }
